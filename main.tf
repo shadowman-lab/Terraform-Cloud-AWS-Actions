5819ff2e6cca4779945f2a352726852f3eecec06
Alex Dworjan <adworjan@redhat.com>
2026-05-19 14:20:04 -0400

    change usage tag

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ resource "aws_instance" "terraformvms" {
       owner: "adworjan"
       env: "dev"
       operating_system: var.rhel_version
-      usage: "shadowmandemos"
+      usage: "shadowmandemo"
       }
 
   lifecycle {
